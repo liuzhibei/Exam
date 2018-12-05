f03fd69d55634fbf9f2bb743c2067522c9689df9
liuzhibei <liuzhibei@xin.com>
2018-12-06 00:23:03 +0800

    'n的平方时间复杂度排序'

--- App/Controller/Algorithm/AlgorithmSorController.php
+++ App/Controller/Algorithm/AlgorithmSorController.php
@@ -5,22 +5,74 @@ namespace App\Controller\Algorithm;
 
 class AlgorithmSorController
 {
-    public static function bubbleSort($arr)
+    /*
+     * 下面四个冒泡算法前三个算法随着顺序优化程度不断提高
+     * ，第四个算法复杂度没有变化只是有些分治思想
+     */
+
+    /*
+     * 普通的冒泡排序
+     */
+    public function bubbleSort ($arr)
     {
-        $length = count($arr);
-        for ($i = 0; $i < $length - 1; $i++) {
+        if (!is_array($arr)) {
+            return false;
+        }
+
+        $length = count($arr); //这四行代码如果当输入的数组大部分情况下是0,1个元素则可以用空间节省时间（直接返回）
+        if ($length <= 1) {    //但是，当大部分情况下是>1个元素时反而多了一次判断（因地制宜）
+            return $arr;
+        }
+
+        for ($i = 0; $i < $length - 1; $i++) { //当条件为$i < $length时结果也对，只是多执行了两次判断
             for ($j = 0; $j < $length - $i - 1; $j++) {
-                if ($arr[$j] > $arr[$j+1]) {
+                if ($arr[$j] > $arr[$j + 1]) {
                     $tmp = $arr[$j];
-                    $arr[$j] = $arr[$j+1];
-                    $arr[$j+1] = $tmp;
+                    $arr[$j] = $arr[$j + 1];
+                    $arr[$j + 1] = $tmp;
                 }
+            }
+        }
+
+        return $arr;
+
+    }
+
+    /*
+     * 优化的冒泡排序(第二层的对比次数都是第一层上一次的个数减1次）
+     */
+    public function bubbleSort2 ($arr)
+    {
+        if (!is_array($arr)) {
+            return false;
+        }
+
+        $length = count($arr);
+        if ($length <= 1) {
+            return $arr;
+        }
+
+        for ($i = 0; $i <= $length - 2; $i++) {
+            $flag = 0;
+            for ($j = 0; $j <= $length - $i -2; $j++) {
+                if ($arr[$i] > $arr[$i + 1]) {
+                    $flag = 1;
+                    $tmp = $arr[$i];
+                    $arr[$i] = $arr[$i + 1];
+                    $arr[$i + 1] = $tmp;
+                }
+            }
+            if ($flag == 0) {
+                break;
             }
         }
         return $arr;
     }
 
-    public static function bubbleSortTwo($arr)
+    /*
+     * 经过更加优化的冒泡排序(第二层的对比次数会越来越少)
+     */
+    public static function bubbleSor2($arr)
     {
         $length = count($arr);
         $i = $length - 1;
@@ -39,7 +91,10 @@ class AlgorithmSorController
         return $arr;
     }
 
-    public static function bubbleSortThree($arr)
+    /*
+     * 这种虽然有分治法的思想但是并没有得到优化
+     */
+    public static function bubbleSort3($arr)
     {
         $length = count($arr);
         $low = 0;
@@ -65,7 +120,72 @@ class AlgorithmSorController
         return $arr;
     }
 
-    public static function quickSort(&$arr, $low, $high)
+    /*
+     * 下面是选择排序
+     */
+    public static function selectSort ($arr)
+    {
+        if (!is_array($arr)) {
+            return false;
+        }
+
+        $length = count($arr);
+        if ($length <= 1) {
+            return $arr;
+        }
+
+        for ($i = 0; $i < $length - 1; $i++) {
+            $max = $i;
+            for ($j = $i; $j < $length - 1; $j++) {
+                if ($arr[$j] < $arr[$j+1]) {
+                    $max = $j + 1;
+                }
+            }
+            if ($max != $i) {
+                $tmp = $arr[$max];
+                $arr[$max] = $arr[$i];
+                $arr[$i] = $tmp;
+            }
+        }
+        return $arr;
+    }
+
+    /*
+     * 下面为插入排序
+     */
+    public static function insertSort ($arr)
+    {
+        if (!is_array($arr)) {
+            return false;
+        }
+
+        $length = count($arr);
+        if ($length <= 1) {
+            return $arr;
+        }
+
+        for ($i = 1; $i < $length; $i++) {
+            $insert = $arr[$i];
+            for ($j = $i; $j >=0; $j--) {
+                if ($arr[$j - 1] > $arr[$j]) {
+                    $arr[$j] = $arr[$j - 1];
+                } else {
+                    $arr[$j] = $insert;
+                    break;
+                }
+            }
+        }
+
+        return $arr;
+    }
+
+    /*
+     * 快速排序
+     */
+    public static function quickSort ($arr) {
+
+    }
+    public static function quickSort2(&$arr, $low, $high)
     {
         if ($low < $high) {
             $middle = self::partision($arr, $low, $high);
